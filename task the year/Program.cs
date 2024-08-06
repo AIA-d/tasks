e5f36a0ebadd335dc0fc0e1990149ce9a436d8a9
i-ano <i-ano@DESKTOP-BN3UOL0>
2024-08-07 00:27:21 +0300

    Добавьте файлы проекта.

---
+++ task the year/Program.cs
@@ -0,0 +1,15 @@
+﻿ Console.WriteLine($"Введите год в формате \"yyyy\" ");
+
+int year = Convert.ToInt32(Console.ReadLine());
+int remains1 = year % 400;
+int remains2 = year % 4;
+int remains3 = year % 100;
+
+if ((remains1 == 0) || (remains2 == 0 && remains3 != 0))
+{
+    Console.WriteLine("Количество дней 366");
+}
+else
+{
+    Console.WriteLine("Количество дней 365");
+}
